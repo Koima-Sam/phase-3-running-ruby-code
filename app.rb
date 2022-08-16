64d4db75931a59b75392c3564ff8d7839809f015
Koima-Sam <sam17koima@gmail.com>
2022-08-16 22:23:59 +0300

    Done.

---
+++ app.rb
@@ -0,0 +1,12 @@
+#This is a comment
+puts ("Hello World!")
+puts ("Hello World!")
+puts ("Hello World!")
+
+#displaying data that are not strings
+p([1,2,3])
+
+#handling complex data
+pp ([{ id: 1, hello: "World" }, { id: 2, hello: "Ruby" }, { id: 3, hello: "Moon" }, { id: 4, hello: "Learner" }])
+
+print "Pass this test, please." 
